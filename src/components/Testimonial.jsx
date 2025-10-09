92703f9d2240fa2fab5edf145196a2b9a133e7e3
ItsZimaaa <Mukhammadkodir7070@Icloud.com>
2025-10-09 12:59:35 +0500

    modified:   src/components/Testimonial.jsx modified:   src/index.css

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -5,9 +5,9 @@ import { MdOutlineStarBorderPurple500, MdOutlineStarPurple500 } from "react-icon
 
 export default function Testimonial() {
     return (
-        <div>
-            <div>
-                <span>
+        <div className="flex justify-center">
+            <div className="flex w-[700px] flex-col items-center gap-6 text-center mt-20 mb-20">
+                <span className="flex flex-col items-center gap-4">
                     <h1 className='heading2'>Testimonial</h1>
                     <h2 className='heading'>Making Food great again and again</h2>
                 </span>
@@ -18,16 +18,16 @@ export default function Testimonial() {
 
                 <img src={Profile} className="rounded-full w-[75px] h-[75px] object-cover" />
 
-                <div>
-                    <MdArrowBackIos />
+                <div className="flex items-center gap-8">
+                    <MdArrowBackIos className="text-5xl" />
                     <p className="paragraph mt-4">
                         You need not only Just Food Stalls with Persons but also specialized equipment, Skills to manage Customers, Effective Product catlogues etc very successful to make your.
                     </p>
-                    <MdArrowForwardIos />
+                    <MdArrowForwardIos className="text-5xl" />
                 </div>
 
                 <span className="flex text-[28px] text-[#DC780B]">
-                    <MdOutlineStarPurple500 />
+                    <MdOutlineStarPurple500 /> 
                     <MdOutlineStarPurple500 />
                     <MdOutlineStarPurple500 />
                     <MdOutlineStarPurple500 />
