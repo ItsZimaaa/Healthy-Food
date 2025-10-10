43283d3bf21b68a2a3a35465e3303582ea2e23ad
ItsZimaaa <Mukhammadkodir7070@Icloud.com>
2025-10-10 16:52:07 +0500

    modified:   src/components/Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from "./../assets/logo.png"
 import { TiThMenu } from "react-icons/ti";
 import { FaPhone } from "react-icons/fa6";
+import { FaTimes } from "react-icons/fa";
 
 export default function Navbar() {
+
+    const [IsOpen, setIsOpen] = useState(false);
+
     return (
         <nav className='w-full flex justify-between items-center paddingY-2 py-5 bg-white position: sticky top-0 z-10'>
 
-            <button className='md:hidden text-[18px] hover:text-[#DC780B]'><TiThMenu /></button>
+            <button
+                className='md:hidden text-[18px] hover:text-[#DC780B]'
+                onClick={() => setIsOpen(!IsOpen)}>
+                {IsOpen ? <FaTimes /> : <TiThMenu />}
+            </button>
+
+            {IsOpen && (
+                <div className="absolute top-[70px] left-0 w-[100%] list-none rounded-3xl bg-white md:hidden flex flex-col items-start px-20 text-2xl gap-5 py-10 text-gray-600 font-semibold">
+                    <li className="hover:text-[#DC780B] cursor-pointer">Home</li>
+                    <li className="hover:text-[#DC780B] cursor-pointer">About us</li>
+                    <li className="hover:text-[#DC780B] cursor-pointer">Menu</li>
+                    <li className="hover:text-[#DC780B] cursor-pointer">Features</li>
+                    <li className="hover:text-[#DC780B] cursor-pointer">Contact us</li>
+                    <button className="button mt-3">Book now</button>
+                </div>
+            )}
 
             <div className='flex items-center gap-10'>
                 <img src={logo} alt="Logo" className="w-[45px]" />
